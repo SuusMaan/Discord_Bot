2cadb33ff1c16089ecd44beb4368bffc6b6f725e
SuusMaan <ilyaserzhantov@yandex.ru>
2025-03-19 18:33:39 +0300

    Create Host version

--- bot.py
+++ bot.py
@@ -1,9 +1,15 @@
+import os
+from dotenv import load_dotenv
 import discord
 from discord.ext import commands
-import os
-from utils import config  # Импортируем настройки из utils/config.py
-import asyncio  # Импортируем модуль asyncio
-import traceback  # Импортируем traceback
+import asyncio
+import traceback
+from flask import Flask
+from threading import Thread
+
+# Загрузка переменных окружения
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
 
 # Настройка intents
 intents = discord.Intents.default()
@@ -12,9 +18,13 @@ intents.members = True  # Если нужны события, связанные
 intents.guilds = True  # Добавляем guilds intent
 
 # Создание бота
-bot = commands.Bot(command_prefix=config.COMMAND_PREFIX, intents=intents)
+bot = commands.Bot(command_prefix="!", intents=intents)
 
-def create_red_embed(title=None, description=None, fields=None, image_url=None):
+
+def create_red_embed(title=None,
+                     description=None,
+                     fields=None,
+                     image_url=None):
     """Создает красный embed."""
     embed = discord.Embed(color=discord.Color.red())
     if title:
@@ -27,6 +37,7 @@ def create_red_embed(title=None, description=None, fields=None, image_url=None):
     if image_url:
         embed.set_image(url=image_url)
     return embed
+
 
 # Загрузка Cogs
 async def load_extensions():
@@ -49,7 +60,9 @@ async def load_extensions():
                         await bot.load_extension(f"cogs.{folder}.{cog_name}")
                         print(f"Loaded cog: cogs.{folder}.{cog_name}")
                     except Exception as e:
-                        print(f"Failed to load extension cogs.{folder}.{cog_name}: {e}")
+                        print(
+                            f"Failed to load extension cogs.{folder}.{cog_name}: {e}"
+                        )
                         traceback.print_exc()
 
 
@@ -59,36 +72,62 @@ async def on_message(message):
     if message.author == bot.user:
         return
 
-    if message.content.startswith(config.COMMAND_PREFIX): # Используем префикс из config.py
+    if message.content.startswith("!"):  # Используем префикс из config.py
         ctx = await bot.get_context(message)
         if ctx.command is None:
             embed = create_red_embed(
                 title="Неизвестная команда",
-                description=f"Такой команды не существует. Используйте `{config.COMMAND_PREFIX}help` для просмотра списка доступных команд."
+                description=
+                f"Такой команды не существует. Используйте `!help` для просмотра списка доступных команд."
             )
             await ctx.send(embed=embed)
 
     await bot.process_commands(message)  # Обрабатываем команды как обычно
 
+
 # Обработчик on_interaction
 @bot.event
 async def on_interaction(interaction: discord.Interaction):
     try:
-        await interaction.response.defer()  # Подтверждаем получение взаимодействия
+        await interaction.response.defer(
+        )  # Подтверждаем получение взаимодействия
     except discord.errors.NotFound:
         print("Interaction not found")  # Interaction больше не существует
+
 
 # Событие on_ready
 @bot.event
 async def on_ready():
     print(f'Бот {bot.user.name} подключился к Discord!')
 
-# Запуск бота
+
+# Flask для "пробуждения" бота
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return "Bot is online!"
+
+
+def run():
+    app.run(host="0.0.0.0", port=8080)
+
+
+def keep_alive():
+    t = Thread(target=run)
+    t.start()
+
+
+# Запуск Flask
+keep_alive()
+
+
 async def main():
     async with bot:
         await load_extensions()
-        await bot.start(config.TOKEN)  # Используем токен из utils/config.py
+        await bot.start(TOKEN)
+
 
 if __name__ == "__main__":
-    import asyncio
     asyncio.run(main())
